582086adb83465579dde2d14c4cec168260b0843
Nikita Konovalov <n.konovalov@galament.com>
2020-12-14 21:16:16 +0000

    add Task model test

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,8 +1,13 @@
 TYPES = %w[Developer Manager Admmin]
+STATES = %w[new_task in_development in_qa in_code_review ready_for_release released archived]
 
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password] do |n|
+  sequence :string, aliases: [:first_name, :last_name, :password, :name] do |n|
     "string#{n}"
+  end
+  
+  sequence :text, aliases: [:description] do |n|
+    "text#{n}"
   end
 
   sequence :email do |n|
@@ -15,5 +20,13 @@ FactoryBot.define do
 
   sequence :type do
     TYPES.sample
+  end
+
+  sequence :state do
+    STATES.sample
+  end
+
+  sequence :expired_at do |n|
+    n.days.after
   end
 end
