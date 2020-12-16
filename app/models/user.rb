cfb6dfa310ad3dceedc5e948d6f9f7ef588d58d5
Nikita <mipo4t@gmail.com>
2020-12-16 19:45:44 +0000

    add models validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
 
   has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
   has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
+
+  validates :first_name, presence: true,
+                         length: { minimum: 2 }
+
+  validates :last_name, presence: true,
+                        length: { minimum: 2 }
+
+  validates :email, presence: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
 end
