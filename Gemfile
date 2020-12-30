5975fb4a44195f8955bcc8991341c8568ad3a89c
Nikita <mipo4t@gmail.com>
2020-12-30 17:04:03 +0400

    fix board

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'kaminari'
 gem 'ransack', github: 'activerecord-hackery/ransack'
 gem 'responders'
 gem 'active_model_serializers'
+gem 'webpacker-react', "~> 1.0.0.beta.1"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ require('channels');
 
 import 'material-design-lite/material.js';
 import WebpackerReact from 'webpacker-react';
-import TaskBoard from '../components/TaskBoard';
+import TaskBoard from 'components/TaskBoard';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
