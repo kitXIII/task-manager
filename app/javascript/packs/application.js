fcf4b5d26337df2a36daa40f4df5323f315f2f08
Nikita <mipo4t@gmail.com>
2020-12-30 12:07:04 +0400

    add react-kanban package and include it

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("@rails/activestorage").start()
 require("channels")
 
 import 'material-design-lite/material.js';
+import WebpackerReact from 'webpacker-react'
+import TaskBoard from '../components/TaskBoard'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +17,5 @@ import 'material-design-lite/material.js';
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+WebpackerReact.setup({ TaskBoard })
