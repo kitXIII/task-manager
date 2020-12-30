c929f09fcc018504d25f2bc2c4cad7a55f33df1c
Nikita <mipo4t@gmail.com>
2020-12-30 15:53:55 +0400

    apply js linting

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("channels")
+require('@rails/ujs').start();
+require('@rails/activestorage').start();
+require('channels');
 
 import 'material-design-lite/material.js';
-import WebpackerReact from 'webpacker-react'
-import TaskBoard from '../components/TaskBoard'
+import WebpackerReact from 'webpacker-react';
+import TaskBoard from '../components/TaskBoard';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -18,4 +18,4 @@ import TaskBoard from '../components/TaskBoard'
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-WebpackerReact.setup({ TaskBoard })
+WebpackerReact.setup({ TaskBoard });
