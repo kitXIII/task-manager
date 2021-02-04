2b2a6fb3c77d01999edc53a176f1a2a1a79f6e89
Nikita <mipo4t@gmail.com>
2021-02-04 22:49:03 +0400

    add letter_opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :letter_opener_web
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,6 @@ Rails.application.routes.draw do
       resources :users, only: [:index, :show]
     end
   end
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
