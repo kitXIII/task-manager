9e1de0af7ca823d6c1c6d972d12278059c3b9868
Nikita <mipo4t@gmail.com>
2021-02-09 02:14:48 +0400

    add production mailer host option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+config.action_mailer.default_url_options = { host: 'kit-board.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name: ENV['MAILER_USERNAME'],
