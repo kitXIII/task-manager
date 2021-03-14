d38077802e4e82feb4ab4b60f58423f82fd75517
Nikita <mipo4t@gmail.com>
2021-03-14 10:33:10 +0400

    fix tasks image eager loading n+1

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -6,7 +6,8 @@ class Api::V1::TasksController < Api::V1::ApplicationController
       ransack(ransack_params).
       result.
       page(page).
-      per(per_page)
+      per(per_page).
+      with_attached_image
 
     respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
   end
